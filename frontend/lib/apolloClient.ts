977124e8fa14cdf9f510da83d9da16e03b2fe284
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2025-11-20 19:59:14 +0100

    [UPDATE] credentials includes

--- frontend/lib/apolloClient.ts
+++ frontend/lib/apolloClient.ts
@@ -1,7 +1,12 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_SERVER_URI + "/query",
+  credentials: 'include' // send cookies if needed
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.NEXT_PUBLIC_SERVER_URI+"/query" }),
+  link: httpLink,
   cache: new InMemoryCache()
 });
 
